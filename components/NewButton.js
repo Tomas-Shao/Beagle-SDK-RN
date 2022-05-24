700f9d38b31b573eef840d2f4b1280a423b2f18b
tomas.shao <zeliang.shao@gmail.com>
2022-05-24 18:04:17 +0800

    web3

--- components/NewButton.js
+++ components/NewButton.js
@@ -4,20 +4,24 @@ import Web3 from 'web3';
 import {Text} from 'react-native';
 
 class NewButton extends Component {
-  componentDidMount() {
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'),
-    );
-    console.log('WEB3 ============================');
-    web3.eth.getBlock('latest').then(function(value) {
-      console.log(value);
-    }).catch(error => console.log(error));
-    console.log('WEB3 ============================');
-  }
+    async componentDidMount() {
+        const web3 = new Web3(
+            new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'),
+        );
+        console.log('WEB3 ============================');
+        web3.eth.getBlock('latest').then(function (value) {
+            console.log(value);
+        }).catch(error => console.log(error));
+        console.log('WEB3 ============================');
 
-  render() {
-    return <Text>Hello World 4444</Text>;
-  }
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts)
+        console.log('WEB4 ============================');
+    }
+
+    render() {
+        return <Text>Hello World 444422</Text>;
+    }
 }
 
 export default NewButton;
