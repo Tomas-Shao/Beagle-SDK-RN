bc75c97a0b92be971539ed6c6857f9dff23cf8e5
tomas.shao <zeliang.shao@gmail.com>
2022-05-27 15:02:12 +0800

    refine code

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -3,22 +3,15 @@ import {Button, Text, View} from 'react-native';
 import Web3 from 'web3';
 
 function HomePage({navigation}) {
-    const web3 = new Web3(
-        new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'),
-    );
-
-    React.useEffect(() => {
-        console.log('use effect in home page');
-        web3.eth.getChainId((error, version) => {
-            // console.error(error)
-            console.log('get chainID in the callback: ', version);
-        }).then(r => {
-            console.log('get chainID in the then: ', r);
-        });
-        return () => {
-            console.log('clean up for use effect');
-        };
-    })
+    // React.useEffect(() => {
+    //     console.log('use effect in home page');
+    //     web3.eth.getChainId().then(r => {
+    //         console.log('get chainID in the then: ', r);
+    //     });
+    //     return () => {
+    //         console.log('clean up for use effect');
+    //     };
+    // })
 
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -26,16 +19,24 @@ function HomePage({navigation}) {
                 Detail Screen
             </Text>
             <Button title="Go to Detail page" onPress={() => navigation.navigate('Detail')}></Button>
-            <Button title="Fetch" onPress={fetchAccount}></Button>
+            <Button title="getLatestBlock" onPress={getLatestBlock}></Button>
+            <Button title="Get chainID" onPress={getChainId}></Button>
+
         </View>
     );
 }
 
-async function fetchAccount() {
-    const web3 = new Web3(
-        new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'),
-    );
-    console.log('fetchAccount begin ============================');
+function getChainId() {
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'));
+    console.log('get chainID');
+    web3.eth.getChainId().then((value) => {
+        console.log('ChainID:', value);
+    });
+}
+
+function getLatestBlock() {
+    const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'));
+    console.log('getLatestBlock begin ============================');
     web3.eth.getBlock('latest').then(function (value) {
         console.log('===================1=================');
         console.log(value);
@@ -43,9 +44,7 @@ async function fetchAccount() {
         console.debug('value: ', value.transactions);
         console.log('===================3=================');
     }).catch(error => console.log(error));
-    console.log('fetchAccount done ============================');
+    console.log('getLatestBlock done ============================');
 }
-
-
 
 export default HomePage;
