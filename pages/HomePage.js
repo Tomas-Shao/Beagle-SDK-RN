7a5474778e585c1c4bc96e511f6185fc7209c00f
tomas.shao <zeliang.shao@gmail.com>
2022-05-28 17:40:22 +0800

    more readable ui

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,20 +1,11 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, useColorScheme, View, StyleSheet} from 'react-native';
 import Web3 from 'web3';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function HomePage({navigation}) {
-    // React.useEffect(() => {
-    //     console.log('use effect in home page');
-    //     web3.eth.getChainId().then(r => {
-    //         console.log('get chainID in the then: ', r);
-    //     });
-    //     return () => {
-    //         console.log('clean up for use effect');
-    //     };
-    // })
-
     const [chainId, setChainId] = useState('null');
-    const [owner, setOwner] = useState('')
+    const [owner, setOwner] = useState('');
 
     function getChainId() {
         const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'));
@@ -236,11 +227,24 @@ function HomePage({navigation}) {
             },
         ], '0x98325eDBE53119bB4A5ab7Aa35AA4621f49641E6');
 
-        console.log(contract.methods)
+        console.log(contract.methods);
         contract.methods.owner('0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2').call().then((value) => {
-            console.log("Get Owner:", value)
-            setOwner(value)
-        })
+            console.log('Get Owner:', value);
+            setOwner(value);
+        });
+    }
+
+    function getLatestBlock() {
+        const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'));
+        console.log('getLatestBlock begin ============================');
+        web3.eth.getBlock('latest').then(function (value) {
+            console.log('===================1=================');
+            console.log(value);
+            console.log('===================2=================');
+            console.debug('value: ', value.transactions);
+            console.log('===================3=================');
+        }).catch(error => console.log(error));
+        console.log('getLatestBlock done ============================');
     }
 
     return (
@@ -250,25 +254,48 @@ function HomePage({navigation}) {
             </Text>
             <Button title="Go to Detail page" onPress={() => navigation.navigate('Detail')}></Button>
             <Button title="getLatestBlock" onPress={getLatestBlock}></Button>
-            <Button title="Get chainID" onPress={getChainId}></Button>
-            <Text>{chainId}</Text>
-            <Button title="Fetch owner: addr.reverse" onPress={fetchOwner}></Button>
-            <Text>{owner}</Text>
+            <Section title="Get ChainId">
+                Click <Button title="HERE" onPress={getChainId}></Button> to fetch chain id <Text style={styles.highlight}>{chainId}</Text>
+            </Section>
+            <Section title="Fetch Owner: addr.reverse">
+                Click <Button title="HERE" onPress={fetchOwner}></Button> to fetch owner <Text style={styles.highlight}>{owner}</Text>
+            </Section>
         </View>
     );
 }
 
-function getLatestBlock() {
-    const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/84ae00fec54f4d65bd1c0505b0e96383'));
-    console.log('getLatestBlock begin ============================');
-    web3.eth.getBlock('latest').then(function (value) {
-        console.log('===================1=================');
-        console.log(value);
-        console.log('===================2=================');
-        console.debug('value: ', value.transactions);
-        console.log('===================3=================');
-    }).catch(error => console.log(error));
-    console.log('getLatestBlock done ============================');
-}
+const styles = StyleSheet.create({
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+        alignSelf: 'flex-start',
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+});
+
+const Section = ({children, title}): Node => {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+        <View style={styles.sectionContainer}>
+            <Text style={[styles.sectionTitle, {color: isDarkMode ? Colors.white : Colors.black}]}>
+                {title}
+            </Text>
+            <Text style={[styles.sectionDescription, {color: isDarkMode ? Colors.light : Colors.dark}]}>
+                {children}
+            </Text>
+        </View>
+    );
+};
 
 export default HomePage;
